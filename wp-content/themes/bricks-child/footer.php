109aebb605644f93426ac6e83f7968c34da7d0ea
jasenwyatt <jasen.wyatt@gmail.com>
2024-10-13 15:03:17 -0400

    adds hard-coded state of header and footer;

--- wp-content/themes/bricks-child/footer.php
+++ wp-content/themes/bricks-child/footer.php
@@ -1,6 +1,8 @@
 <?php namespace Bricks; ?>
 
-<footer>Site Footer</footer>
+<footer class="bg-red-500 p-6 lg:px-8">
+    <p class="text-sm text-red-100">Copyright &copy; <?php echo date('Y'); ?> Two Guys From Italy &bull; All rights reserved.</p>
+</footer>
 
 <?php
 do_action( 'bricks_before_footer' );
